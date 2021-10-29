34ef46c622794f232f0a57c98435ca87df6a3ac1
vvvMrRobotvvv <jakubskoroch@gmail.com>
2021-10-29 22:55:08 +0200

    comit

---
+++ test22.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <limits>
+
+using namespace std;
+
+void menu()
+{
+	cout << "\n1) Wczytaj" << endl;
+}
+
+int menu_switch()
+{
+	int a;
+	do
+	{
+		if(cin.fail())
+		{
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		cin >> a;
+	} while (cin.fail() || (a < 0) && (a > 2));
+	return a;
+}
+
+void wczytywanie(string t[], int ile)
+{
+	int a;
+	cout << "\nPodaj ile komorek chcesz wczytac: ";
+	cin >> a;
+	for(int i; i < a; i++)
+	{
+		cout << "Podaj komorke " << ile + 1 << ": ";
+		getline(cin, t[ile]);
+	}
+}
+
+int main()
+{
+	int ile = 0;
+	menu();
+	int selection = menu_switch();
+	string tablica[10];
+	switch (selection)
+	{
+	case 1:
+		wczytywanie(tablica, ile);
+		break;
+	
+	default:
+		break;
+	}
+}
