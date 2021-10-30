9725492bf88fd26bafb626ce6712d07fd39c6a64
vvvMrRobotvvv <jakubskoroch@gmail.com>
2021-10-31 01:19:27 +0200

    comit

--- test22.cpp
+++ test22.cpp
@@ -4,10 +4,27 @@
 
 using namespace std;
 
-void mene()
+int podaj(int &a)
+{
+	cin >> a;
+	return a;
+}
+
+bool czy()
+{
+	if (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		return false;
+	}
+	return true;
+}
+
+void menu()
 {
 	cout << "Menu glowne:\n";
-	cout << "Pocja 1\n";
+	cout << "Pocja 1 wypisz\n";
 	cout << "Pocja 2\n";
 	cout << "Pocja 3\n";
 	cout << "Pocja 0" << endl;
@@ -15,10 +32,26 @@ void mene()
 
 int main()
 {
+	string tablica[10];
+	int a;
 	do
 	{
-		
+		a = -1;
+		do
+		{
+			menu();
+			podaj(a);
+			cout << endl;
+		} while (czy() == false || a < 0 || a > 3);
+
+		switch (a)
+		{
+		case 1:
+			//funkcja
+			break;
+		default:
+			break;
+		}
 	} while (a != 0);
-	
 	return 0;
 }
