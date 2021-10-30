7da54334dd7da1622a45f5c388688f7734718889
vvvMrRobotvvv <jakubskoroch@gmail.com>
2021-10-31 00:47:16 +0200

    comit update test22

---
+++ test22.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <limits>
+
+using namespace std;
+
+void mene()
+{
+	cout << "Menu glowne:\n";
+	cout << "Pocja 1\n";
+	cout << "Pocja 2\n";
+	cout << "Pocja 3\n";
+	cout << "Pocja 0" << endl;
+}
+
+int main()
+{
+	do
+	{
+		
+	} while (a != 0);
+	
+	return 0;
+}
