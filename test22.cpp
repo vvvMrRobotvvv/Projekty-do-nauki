d25a340c85e8cac81011032731888aa15b820164
vvvMrRobotvvv <jakubskoroch@gmail.com>
2021-10-31 02:05:36 +0200

    comit niedokonczone test22

--- test22.cpp
+++ test22.cpp
@@ -30,9 +30,28 @@ void menu()
 	cout << "Pocja 0" << endl;
 }
 
+void wypisz(string t[], int &ile)
+{
+	int x;
+	cout << "\nIle komorek chcesz podac ";
+	cin >> x;
+	while (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "\nIle komorek chcesz podac ";
+		cin >> x;
+	}
+	for (int i = ile; i < (x + ile - 1); i++)
+	{
+		cout << t[i] << endl;
+	}
+}
+
 int main()
 {
 	string tablica[10];
+	int ile = 0;
 	int a;
 	do
 	{
@@ -47,7 +66,7 @@ int main()
 		switch (a)
 		{
 		case 1:
-			//funkcja
+			wypisz(tablica, ile);
 			break;
 		default:
 			break;
